c15ccf2d05d6785c5d36d078cb85e055a35cb6c5
Smalldice123 <Nzeludonald@gmail.com>
2025-03-03 02:10:05 +0100

    first

--- latest/vite.config.js
+++ latest/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
@@ -10,4 +11,4 @@ export default defineConfig({
   server: {
     historyApiFallback: true,  // Ensures React Router works
   },
-});
+})
